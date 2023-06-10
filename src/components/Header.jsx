1c1b41b6d24c7f9b747ebcb80012da2ef053ff7f
mknyambura <kariuki.mercie@gmail.com>
2023-06-10 20:58:58 +0300

    responsive navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import logo from '../images/logo.png'
 import { Link } from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
+
 
 function Header() {
+  const [open, setOpen] = useState(false)
+  const [isClicked, setIsClicked] = useState(false)
+  
+  const handleclick =() => {
+    setOpen(!open)
+  }
+
   return (
     <div>
-        <div className='flex flex-row justify-between items-center m-16'>
+        {/* <div className='flex flex-row justify-between items-center m-16'>
             <div className='flex flex-row gap-6'>
-                <img src={logo} alt="" />
-                <Link to='/' className='font-bold text-xl'>Dentalist</Link>
             </div>
             <div className='flex flex-row gap-8 font-semibold'>
-                <Link to='/about-us'>About Us</Link>
-                <h1>New Patient</h1>
-                <Link to='/services'>Our Services</Link>
                 <Link to='/contact-us'>Contact Us</Link>
             </div>
-            <button className='bg-[#583FBC] px-8 py-2 rounded-lg text-white font-bold'>Book appointment</button>
+            
+        </div> */}
+        
+
+
+        <div className='lg:flex mg:flex-row justify-between items-center py-12'>
+        <div className='font-bold text-3xl sm:px-16'>
+          <img src={logo} alt="" />
+          <Link to='/' className='font-bold text-xl'>Dentalist</Link>
         </div>
+        <div className='absolute right-8 top-[5%] lg:hidden' onClick={handleclick}>
+          {
+            open ?  <FaBars className='text-[#583FBC] text-3xl'/> : <FaTimes className='text-[#583FBC] text-3xl'/>
+          }
+        </div>
+        <ul className={`sm:w-full md:w-full lg:w-auto sm:z-10 md:z-10 z-[-1] sm:absolute md:absolute lg:static sm:py-6 sm:px-6 sm:mb-6 sm:mt-6 md:py-6 md:px-6 md:mb-6 md:mt-6 sm:bg-[#583FBC] md:bg-[#583FBC] lg:bg-transparent flex sm:flex-col md:flex-col lg:flex-row lg:gap-14 sm:gap-8 md:gap-8 items-center ${open ? "top-[-1090px]": "top-auto"}`}>
+          <li className='opacity-70 hover:font-bold active:text-[#583FBC] active:underline hover:cursor-pointer'>
+            <a href="/about-us">About Us</a>
+          </li>
+          <li className='opacity-70 hover:font-bold active:text-[#583FBC] active:underline hover:cursor-pointer'>New Patient</li>
+          <li className='opacity-70 hover:font-bold active:text-[#583FBC] active:underline hover:cursor-pointer'>
+            <a href="/services">Our Services</a>
+          </li>
+          <li className='opacity-70 hover:font-bold active:text-[#583FBC] active:underline hover:cursor-pointer'>
+            <a href="/contact-us">Contact Us</a>
+          </li>
+          <div className='flex flex-row gap-4 '>
+            <button className='bg-[#583FBC] px-8 py-2 rounded-lg text-white font-bold'>Book appointment</button>
+          </div>
+        </ul>
+      </div>
     </div>
   )
 }
